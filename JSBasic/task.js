98e3ededc287eeba743c7c36a66756860eafc128
DESKTOP-7GSN2KQ\ASUS <samovolkinmaxim.null@gmail.com>
2018-08-21 18:37:55 +0400

    Доделаны все задания по JSBasic

--- JSBasic/task.js
+++ JSBasic/task.js
@@ -112,7 +112,6 @@ console.log("Проверка some():");
 console.log("some([NaN, 7, NaN], Number.isNaN) -> " + some([NaN, 7, NaN], Number.isNaN));
 
 
-
 function multiplyOrThrow(a, b) {
     if (Math.random() < 0.5) {
         return a * b;
@@ -120,6 +119,7 @@ function multiplyOrThrow(a, b) {
         throw 'MultiplicatorUnitFailure';
     }
 }
+
 
 function tryMultiply() {
     var trying = 0;
@@ -169,8 +169,86 @@ function differenceInYears(dateA, dateB) {
 console.log("Проверка differenceInYears():");
 console.log("today - 09.01.1998 = " + differenceInYears(new Date(1998, 0, 8), new Date()));
 
+
 function replaceQuotes(str) {
     return str.replace(/'(\w*)'/g, '"$1"');
 }
 console.log("Провека replaceQuotes():");
 console.log("I'm a 'man'. I'm the 'guardian' -> " + replaceQuotes("I'm a 'man'. I'm the 'guardian'"));
+
+
+function findNumbers(arr) {
+    var pattern = /^(?!\.*([eE][+-]?\d+)*$)([+-]?(?=\d+)\d*)?\.?\d*([eE][+-]?\d+)?$/;
+    
+    return arr.filter(number => pattern.test(number));
+}
+console.log("Проверка функции findNumbers:");
+console.log("findNumbers([\"\.1\", \".\", \"+.\", \"+1.\", \"+\", \"+23\", \"1.3E-4\", \"E-4\", \"..\", \"+-\", \"11\", \"\", \"1e2\", \"1.\")");
+console.log(findNumbers([".1", ".", "+.", "+1.", "+", "+23", "1.3E-4", "E-4", "..", "+-", "11", "", "1e2", "1."]));
+
+
+function findAnomaly(arr, param) {
+    var min = arr.reduce(function(previousItem, currentItem) {
+        return (previousItem[param] > currentItem[param])? currentItem: previousItem;
+    }, initialValue=arr[0]);
+
+    var max = arr.reduce(function(previousItem, currentItem) {
+        return (previousItem[param] < currentItem[param])? currentItem: previousItem;
+    }, initialValue=arr[0]);
+    
+    return {min: min, max: max};
+}
+console.log("Проверка функции findAnomaly:");
+console.log("findAnomaly([{a: 2, b: 2}, {f: 33, a: 1}, {a: 87, b: 2}, {a: 45}], \"a\")");
+console.log(findAnomaly([{a: 2, b: 2}, {f: 33, a: 1}, {a: 87, b: 2}, {a: 45}], "a"));
+
+
+let weatherArray = {
+    "Izhevsk": {
+        2017: {
+            0: [1, -14, 3, -5, 5, 0, 7],
+            1: [1, 2, 3, 4, 5, 6, 7],
+            2: [1, 2, 3, 4, 5, 6, 7],
+            3: [1, 2, 34, 4, 5, 6, 7],
+            4: [1, 2, 3, 4, 21, 6, 7],
+            5: [1, 2, 3, 4, 5, 6, 7],
+            6: [1, 32, 15, 40, 20, 23, 7],
+            7: [1, 2, 3, 4, 5, 6, 7],
+            8: [1, 2, 34, 4, 5, 6, 7],
+            9: [1, 2, 3, 4, 4, 6, 7],
+            10: [1, 2, 21, 4, 5, 6, 7],
+            11: [1, 2, 3, 4, 5, 6, 7],
+        },
+        2018: {
+            0: [1, -10, 3, 4, 5, 6, 7],
+            1: [1, 2, 3, 4, 5, 6, 7],
+            2: [1, 71, 3, 4, 5, 6, 7],
+            3: [1, 24, 34, 4, 5, 6, 7],
+            4: [1, 2, 24, 4, 21, 6, 7],
+            5: [1, 2, 3, 32, 5, 6, 7],
+            6: [1, 32, 3, 4, 5, 6, 7],
+            7: [1, 2, 86, 74, 74, 6, 7],
+            8: [1, 2, 34, 4, 5, 6, 7],
+            9: [1, 2, 3, 4, 4, 6, 7],
+            10: [1, 2, 21, 4, 100, 6, 7],
+            11: [1, 2, 3, 4, 5, 6, 7],
+        }
+    }
+};
+
+function weatherStat(arr, city_date) {
+    // arr - Array
+    // city_date - {city: String, date: new Date()}
+    var city = city_date["city"],
+        date = city_date["date"],
+        year = date.getFullYear(),
+        month = date.getMonth();
+    
+    var days = arr[city][year][month];
+    var temperatureSum = days.reduce((accumulator, current) => accumulator + current, initialValue=0);
+
+    return temperatureSum / days.length;
+}
+console.log("Проверка функции weatherStat:");
+console.log("Средняя температура за январь 2017 года: " + weatherStat(weatherArray, {city: "Izhevsk", date: new Date(2017, 0)}));
+console.log("Средняя температура за июль 2017 года: " + weatherStat(weatherArray, {city: "Izhevsk", date: new Date(2017, 6)}));
